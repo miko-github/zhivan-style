730087b0602b117ec50d2dfc5586d5606529f27a
miko-github <mikoloism-github@gmail.com>
2022-06-09 14:37:31 +0430

    feat (atoms) : common element between cards in single component file

---
+++ src/components/atoms/Card.jsx
@@ -0,0 +1,23 @@
+import { createBem as bem, classnames } from '../../utils';
+import { ICart } from '../Icon';
+
+export default function Card({ className }) {
+	const card = bem('card');
+	return (
+		<section className={classnames(className, card())}>
+			<a className={card('link')} href='#/'>
+				click here to visit product
+			</a>
+			<section className={card('thumbnail')}></section>
+			<section className={card('caption')}>
+				<section className={card('details')}>
+					<h3 className={card('title')}>Title</h3>
+					<p className={card('description')}>Description</p>
+				</section>
+				<button type='button' className={card('button')}>
+					<ICart className={card('icon')} />
+				</button>
+			</section>
+		</section>
+	);
+}
