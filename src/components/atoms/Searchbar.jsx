9ae1762609d6bdfad6ee6ca6610847bf918482d2
miko-github <mikoloism-github@gmail.com>
2022-06-09 17:14:24 +0430

    fix (atoms) : remove the old search box component and replace it by new Searchbar component

--- src/components/atoms/Searchbar.jsx
+++ src/components/atoms/Searchbar.jsx
@@ -1,3 +1,20 @@
+import { classnames, createBem as bem } from '../../utils';
+import { ISearch } from './Icon';
+
+// TODO: { useSearch } from '../hooks'; and use the api to fetch the search results and useSearchList to display them
+
 export default function Searchbar() {
-	return <div></div>;
+	const search = bem('searchbar');
+	return (
+		<form className={classnames('searchbar', this.props.className)}>
+			<input
+				type='text'
+				placeholder='جستجوی محصولات'
+				className={search('input')}
+			/>
+			<button className={search('button')} type='button'>
+				<ISearch className={search('icon')} />
+			</button>
+		</form>
+	);
 }
