d81fe4fb2664c2106a2785080c55ee452cf0d0fb
miko-github <mikoloism-github@gmail.com>
2022-06-09 12:34:45 +0430

    chor (component) : delete icon folder component to signle file component
    fix : renaming the chevron[right/left] icons to arrow[right/left] icons
    feat : adding chevron-up icon

---
+++ src/components/atoms/Icon.jsx
@@ -0,0 +1,159 @@
+export const ICart = ({ className }) => (
+	<svg
+		className={className}
+		xmlns='http://www.w3.org/2000/svg'
+		id='icon-cart'
+		fill='none'
+		width='19.5'
+		height='20'
+		stroke='#dfad8a'
+		stroke-width='1.5'
+		viewBox='0 0 19.5 20'>
+		<g transform='translate(-2.25 -1.75)'>
+			<path
+				d='M15.773,9.305V6.273a3.781,3.781,0,0,0-7.562-.017V9.305'
+				fill='none'
+				stroke='inherit'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+				stroke-width='inherit'
+			/>
+			<path
+				d='M16.742,21H7.258A4.256,4.256,0,0,1,3,16.745V11.229A4.256,4.256,0,0,1,7.258,6.974h9.484A4.256,4.256,0,0,1,21,11.229v5.516A4.256,4.256,0,0,1,16.742,21Z'
+				fill='none'
+				stroke='inherit'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+				stroke-width='inherit'
+				fill-rule='evenodd'
+			/>
+		</g>
+	</svg>
+);
+
+export const IArrowLeft = ({ className }) => (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		id='icon-arrow-left'
+		width='72'
+		height='60'
+		viewBox='0 0 72 60'
+		className={className}
+		stroke='#dfad8a'
+		stroke-width='2'>
+		<g transform='translate(-1649 -3618)'>
+			<g transform='translate(1667.75 3632.75)'>
+				<path
+					d='M4.25,12.274H35.777'
+					transform='translate(0 2.979)'
+					fill='none'
+					stroke='inherit'
+					stroke-linecap='round'
+					stroke-linejoin='round'
+					stroke-width='inherit'
+				/>
+				<path
+					d='M13.292,24.257l-9.042-9,9.042-9'
+					transform='translate(0 0)'
+					fill='none'
+					stroke='inherit'
+					stroke-linecap='round'
+					stroke-linejoin='round'
+					stroke-width='inherit'
+				/>
+			</g>
+		</g>
+	</svg>
+);
+
+export const IArrowRight = ({ className }) => (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		width='72'
+		height='60'
+		viewBox='0 0 72 60'
+		className={className}
+		id='icon-arrow-right'
+		stroke='#dfad8a'
+		stoke-width='2'>
+		<g transform='translate(17.473 21)'>
+			<path
+				d='M35.777,12.274H4.25'
+				transform='translate(-4.25 -3.27)'
+				fill='none'
+				stroke='inherit'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+				stroke-width='inherit'
+			/>
+			<path
+				d='M4.25,24.257l9.042-9-9.042-9'
+				transform='translate(18.236 -6.25)'
+				fill='none'
+				stroke='inherit'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+				stroke-width='inherit'
+			/>
+		</g>
+	</svg>
+);
+
+export const IChevronUp = ({ className }) => (
+	<svg
+		className={className}
+		id='icon-chevron-up'
+		xmlns='http://www.w3.org/2000/svg'
+		width='60'
+		height='60'
+		viewBox='0 0 60 60'
+		stroke='#165753'
+		stroke-width='2'>
+		<g transform='translate(-930 -7791)'>
+			<path
+				d='M8.5,5,24.168,20.668,8.5,36.335'
+				transform='translate(939.332 7834.334) rotate(-90)'
+				fill='none'
+				stroke='inherit'
+				stroke-width='inherit'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+			/>
+		</g>
+	</svg>
+);
+
+export const ISearch = ({ className }) => (
+	<svg
+		className={className}
+		id='icon-search'
+		xmlns='http://www.w3.org/2000/svg'
+		width='20.575'
+		height='21.033'
+		stroke='#dfad8a'
+		stroke-width='1.5'
+		viewBox='0 0 20.575 21.033'>
+		<g id='Search' transform='translate(0.75 0.75)'>
+			<circle
+				cx='8.989'
+				cy='8.989'
+				r='8.989'
+				transform='translate(0 0)'
+				fill='none'
+				stroke='inherit'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+				stroke-width='inherit'
+			/>
+			<path
+				d='M18.018,18.485,21.542,22'
+				transform='translate(-2.778 -2.778)'
+				fill='none'
+				stroke='inherit'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+				stroke-width='inherit'
+			/>
+		</g>
+	</svg>
+);
